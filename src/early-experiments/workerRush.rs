34ad1807f85946e59c33ada57d9e5785b2153187
dwin357 <e.r.steinmetz@gmail.com>
2023-10-13 18:38:27 -0700

    Fallback position prior to first experiment

---
+++ src/early-experiments/workerRush.rs
@@ -0,0 +1,16 @@
+use rust_sc2::prelude::*;
+
+#[bot]
+#[derive(Default)]
+struct WorkerRush;
+impl Player for WorkerRush {
+    fn get_player_settings(&self) -> PlayerSettings {
+        PlayerSettings::new(Race::Protoss)
+    }
+    fn on_start(&mut self) -> SC2Result<()> {
+        for worker in &self.units.my.workers {
+            worker.attack(Target::Pos(self.enemy_start), false);
+        }
+        Ok(())
+    }
+}
